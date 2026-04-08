facb333c2fbacc83517b1e5dbf8cde04a5576a08
SzymonKubica <szymo.kubica@gmail.com>
2026-04-08 16:40:19 +0200

    Add documentation to the trinkeycontroller

--- devkit/trinkeycontroller.ino
+++ devkit/trinkeycontroller.ino
@@ -29,7 +29,6 @@ uint8_t const desc_hid_report[] = {TUD_HID_REPORT_DESC_KEYBOARD()};
 #define HIGH_THRESHOLD 900
 #define LOW_THRESHOLD 100
 
-
 uint32_t button_mask = (1UL << BUTTON_X) | (1UL << BUTTON_Y) |
                        (1UL << BUTTON_START) | (1UL << BUTTON_A) |
                        (1UL << BUTTON_B) | (1UL << BUTTON_SELECT);
@@ -42,125 +41,129 @@ Adafruit_seesaw ss(&Wire);
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
 
 // the setup routine runs once when you press reset:
-void setup() {
-  Serial.begin(115200);
+void setup()
+{
+        Serial.begin(115200);
 
-  usb_hid.setReportDescriptor(desc_hid_report, sizeof(desc_hid_report));
-  usb_hid.begin();
+        usb_hid.setReportDescriptor(desc_hid_report, sizeof(desc_hid_report));
+        usb_hid.begin();
 
-  delay(1000); // wait for host
+        delay(1000); // wait for host
 
 #if defined(NEOPIXEL_POWER)
-  // If this board has a power control pin, we must set it to output and high
-  // in order to enable the NeoPixels. We put this in an #if defined so it can
-  // be reused for other boards without compilation errors
-  pinMode(NEOPIXEL_POWER, OUTPUT);
-  digitalWrite(NEOPIXEL_POWER, HIGH);
+        // If this board has a power control pin, we must set it to output and
+        // high in order to enable the NeoPixels. We put this in an #if defined
+        // so it can be reused for other boards without compilation errors
+        pinMode(NEOPIXEL_POWER, OUTPUT);
+        digitalWrite(NEOPIXEL_POWER, HIGH);
 #endif
 
-  pixels.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
-  pixels.setBrightness(20); // not so bright
+        pixels.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+        pixels.setBrightness(20); // not so bright
 
-  Serial.println("Setting up seesaw I2C interface...");
+        Serial.println("Setting up seesaw I2C interface...");
 
-  if (!ss.begin(0x50)) {
-    Serial.println("ERROR! seesaw not found");
-  }
-  Serial.println("seesaw started");
-  uint32_t version = ((ss.getVersion() >> 16) & 0xFFFF);
-  if (version != 5743) {
-    Serial.print("Wrong firmware loaded? ");
-    Serial.println(version);
-  }
+        if (!ss.begin(0x50)) {
+                Serial.println("ERROR! seesaw not found");
+        }
+        Serial.println("seesaw started");
+        uint32_t version = ((ss.getVersion() >> 16) & 0xFFFF);
+        if (version != 5743) {
+                Serial.print("Wrong firmware loaded? ");
+                Serial.println(version);
+        }
 
-  Serial.println("Found Product 5743");
+        Serial.println("Found Product 5743");
 
-  ss.pinModeBulk(button_mask, INPUT_PULLUP);
-  ss.setGPIOInterrupts(button_mask, 1);
-  pinMode(38, INPUT);
+        ss.pinModeBulk(button_mask, INPUT_PULLUP);
+        ss.setGPIOInterrupts(button_mask, 1);
+        pinMode(38, INPUT);
 }
 
-void poll_directional_input() {
-  // Reverse x/y values to match joystick orientation
-  int x = ss.analogRead(14);
-  int y = ss.analogRead(15);
+void poll_directional_input()
+{
+        // Reverse x/y values to match joystick orientation
+        int x = ss.analogRead(14);
+        int y = ss.analogRead(15);
 
-  Serial.println("x: " + String(x) + " y: " + String(y));
+        Serial.println("x: " + String(x) + " y: " + String(y));
 
-  if (x < LOW_THRESHOLD) {
-    uint8_t keycode[6] = {HID_KEY_ARROW_RIGHT};
-    usb_hid.keyboardReport(0, 0, keycode);
-    delay(100);
-    usb_hid.keyboardRelease(0);
-    return;
-  }
-  if (x > HIGH_THRESHOLD) {
-    uint8_t keycode[6] = {HID_KEY_ARROW_LEFT};
-    usb_hid.keyboardReport(0, 0, keycode);
-    delay(100);
-    usb_hid.keyboardRelease(0);
-    return;
-  }
-  if (y < LOW_THRESHOLD) {
-    uint8_t keycode[6] = {HID_KEY_ARROW_UP};
-    usb_hid.keyboardReport(0, 0, keycode);
-    delay(100);
-    usb_hid.keyboardRelease(0);
-    return;
-  }
-  if (y > HIGH_THRESHOLD) {
-    uint8_t keycode[6] = {HID_KEY_ARROW_DOWN};
-    usb_hid.keyboardReport(0, 0, keycode);
-    delay(100);
-    usb_hid.keyboardRelease(0);
-    return;
-  }
+        if (x < LOW_THRESHOLD) {
+                uint8_t keycode[6] = {HID_KEY_ARROW_RIGHT};
+                usb_hid.keyboardReport(0, 0, keycode);
+                delay(100);
+                usb_hid.keyboardRelease(0);
+                return;
+        }
+        if (x > HIGH_THRESHOLD) {
+                uint8_t keycode[6] = {HID_KEY_ARROW_LEFT};
+                usb_hid.keyboardReport(0, 0, keycode);
+                delay(100);
+                usb_hid.keyboardRelease(0);
+                return;
+        }
+        if (y < LOW_THRESHOLD) {
+                uint8_t keycode[6] = {HID_KEY_ARROW_UP};
+                usb_hid.keyboardReport(0, 0, keycode);
+                delay(100);
+                usb_hid.keyboardRelease(0);
+                return;
+        }
+        if (y > HIGH_THRESHOLD) {
+                uint8_t keycode[6] = {HID_KEY_ARROW_DOWN};
+                usb_hid.keyboardReport(0, 0, keycode);
+                delay(100);
+                usb_hid.keyboardRelease(0);
+                return;
+        }
 };
 
-void poll_action_input() {
+void poll_action_input()
+{
 
-  uint32_t buttons = ss.digitalReadBulk(button_mask);
-  // reject impossible results due to I2C delays caused by heavy SPI load.
-  if (buttons == 0 || buttons == 0xFFFFFFFF)
-    return;
+        uint32_t buttons = ss.digitalReadBulk(button_mask);
+        // reject impossible results due to I2C delays caused by heavy SPI load.
+        if (buttons == 0 || buttons == 0xFFFFFFFF)
+                return;
 
-  if (!(buttons & (1UL << BUTTON_A))) {
-    uint8_t keycode[6] = {HID_KEY_R};
-    usb_hid.keyboardReport(0, 0, keycode);
-    delay(100);
-    usb_hid.keyboardRelease(0);
-    return;
-  }
-  if (!(buttons & (1UL << BUTTON_B))) {
-    uint8_t keycode[6] = {HID_KEY_G};
-    usb_hid.keyboardReport(0, 0, keycode);
-    delay(100);
-    usb_hid.keyboardRelease(0);
-    return;
-  }
-  if (!(buttons & (1UL << BUTTON_Y))) {
-    uint8_t keycode[6] = {HID_KEY_B};
-    usb_hid.keyboardReport(0, 0, keycode);
-    delay(100);
-    usb_hid.keyboardRelease(0);
-    return;
-  }
-  if (!(buttons & (1UL << BUTTON_X))) {
-    uint8_t keycode[6] = {HID_KEY_Y};
-    usb_hid.keyboardReport(0, 0, keycode);
-    delay(100);
-    usb_hid.keyboardRelease(0);
-    return;
-  }
+        if (!(buttons & (1UL << BUTTON_A))) {
+                uint8_t keycode[6] = {HID_KEY_R};
+                usb_hid.keyboardReport(0, 0, keycode);
+                delay(100);
+                usb_hid.keyboardRelease(0);
+                return;
+        }
+        if (!(buttons & (1UL << BUTTON_B))) {
+                uint8_t keycode[6] = {HID_KEY_G};
+                usb_hid.keyboardReport(0, 0, keycode);
+                delay(100);
+                usb_hid.keyboardRelease(0);
+                return;
+        }
+        if (!(buttons & (1UL << BUTTON_Y))) {
+                uint8_t keycode[6] = {HID_KEY_B};
+                usb_hid.keyboardReport(0, 0, keycode);
+                delay(100);
+                usb_hid.keyboardRelease(0);
+                return;
+        }
+        if (!(buttons & (1UL << BUTTON_X))) {
+                uint8_t keycode[6] = {HID_KEY_Y};
+                usb_hid.keyboardReport(0, 0, keycode);
+                delay(100);
+                usb_hid.keyboardRelease(0);
+                return;
+        }
 }
 
-void loop() {
+void loop()
+{
 
-  while (true) {
-    if (usb_hid.ready()) {
-      poll_directional_input();
-      poll_action_input();
-      delay(50);
-    }
-  }
+        while (true) {
+                if (usb_hid.ready()) {
+                        poll_directional_input();
+                        poll_action_input();
+                        delay(50);
+                }
+        }
 }
