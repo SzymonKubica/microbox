d93253d02dbae0ba2e9beea67a39e558ee7d00f4
SzymonKubica <szymo.kubica@gmail.com>
2026-05-25 18:38:51 +0200

    Add lopaka backup files

---
+++ lopaka-backup/README.md
@@ -0,0 +1,10 @@
+# Lopaka Backup
+
+This directory contains backup generated images that were
+created using the Lopaka embedded display graphics editor.
+
+That tool unfortuntately only allows to create 3 screens per
+project free of charge, adding more requires a premium subscription.
+
+To get around this, we dump the raw lopaka-generated graphics
+rendering scripts here.
---
+++ lopaka-backup/power_off.cpp
@@ -0,0 +1,12 @@
+
+// [BEGIN lopaka generated]
+tft.fillEllipse(162, 137, 27, 27, 0xBDF7);
+tft.fillEllipse(162, 137, 27, 27, 0xBDF7);
+tft.fillEllipse(162, 137, 15, 15, 0x0);
+tft.fillRect(158, 107, 9, 28, 0xBDF7);
+tft.fillRect(167, 100, 5, 39, 0x0);
+tft.fillEllipse(162, 105, 4, 4, 0xBDF7);
+tft.fillEllipse(162, 134, 4, 4, 0xBDF7);
+tft.fillRect(153, 100, 5, 41, 0x0);
+// [END lopaka generated]
+
